97839ad2def69869c7de9cd3cf778d181daa18cd
rishimash <rishi@open.store>
2022-10-26 19:27:09 -0400

    wand

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def check_password():
     if "password_correct" not in st.session_state:
         # First run, show input for password.
         st.text_input(
-            "Whats the word?", type="password", on_change=password_entered, key="password"
+            "Whats the 🪄 word?", type="password", on_change=password_entered, key="password"
         )
         return False
     elif not st.session_state["password_correct"]:
